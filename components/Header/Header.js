c7ed05b6d45012566b0021cde9deb619b5baeef3
Varun Bankar <mail.varunbankar@gmail.com>
2021-01-11 17:04:22 +0530

    Create navbar

---
+++ components/Header/Header.js
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+export default function Header() {
+    return (
+        <nav className={styles.navbar}>
+            Next.js Blog
+        </nav>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from "../components/Header/Header";
 
 export default function Home() {
     return (
@@ -7,6 +8,7 @@ export default function Home() {
                 <title>Next.js Blog</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
+            <Header />
             <main>
                 <h1 className="mt-4 text-7xl font-bold text-blue-900 text-center">
                     Hello Next.js
